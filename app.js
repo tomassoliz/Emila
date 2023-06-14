d973833a974f0e063ced90a7219ed46b64d2f679
Tomas Soliz <tomassoliz07@gmail.com>
2023-06-14 08:54:52 -0300

    subiendo archivos

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3030;
+
+app.use(express.static('public'))
+
+// sendFile: metodo para devolver un archivo
+
+/* rutas*/
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')))
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'views', 'about.html')))
+app.get('/music', (req, res) => res.sendFile(path.join(__dirname, 'views', 'music.html')))
+app.get("/*",(req,res) => res.sendFile(path.join(__dirname,"views","404notFound.html")))
+
+app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`))
+
+// app.listen(PORT, () => console.log(" Server running in http://localhost:" + PORT))
